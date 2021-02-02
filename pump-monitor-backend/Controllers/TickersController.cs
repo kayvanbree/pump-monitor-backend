57e4be250a0f4c8000dbc831a67a46d5e6e3780f
Kay van Bree <kayvanbree@gmail.com>
2021-02-02 12:01:20 +0100

    First commit

---
+++ pump-monitor-backend/Controllers/TickersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace pump_monitor_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TickersController : ControllerBase
+    {
+        private static readonly string[] Tickers = new[]
+        {
+            "MTHBTC"
+        };
+
+        private readonly ILogger<TickersController> _logger;
+
+        public TickersController(ILogger<TickersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Ticker> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Ticker
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Tickers[rng.Next(Tickers.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
---
+++ pump-monitor-backend/Startup.cs
@@ -0,0 +1,76 @@
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Okta.AspNetCore;
+
+namespace pump_monitor_backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = OktaDefaults.ApiAuthenticationScheme;
+                    options.DefaultChallengeScheme = OktaDefaults.ApiAuthenticationScheme;
+                    options.DefaultSignInScheme = OktaDefaults.ApiAuthenticationScheme;
+                })
+                .AddOktaWebApi(new OktaWebApiOptions()
+                {
+                    OktaDomain = Configuration["Okta:OktaDomain"]
+                });
+
+            // Use [AllowAnonymous] for going into an endpoint raw
+            services.AddMvc(o =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                o.Filters.Add(new AuthorizeFilter());
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                builder => 
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
